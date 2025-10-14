9e762f2ddb00d05f815080b9c1f0309c86904049
Lucas Lantemam Leite <lucaslantemam2005@gmail.com>
2025-10-14 19:57:15 -0300

    feat: cria o mapeamento da tabela

---
+++ GearOps.Api/Data/Mapping/DeviceMap.cs
@@ -0,0 +1,53 @@
+using GearOps.Api.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace GearOps.Api.Data.Mapping;
+
+public sealed class DeviceMap : IEntityTypeConfiguration<Device>
+{
+    public void Configure(EntityTypeBuilder<Device> builder)
+    {
+        builder.ToTable("Devices");
+
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.Name)
+            .HasColumnName("Name")
+            .HasColumnType("varchar(30)")
+            .IsRequired();
+
+        builder.Property(x => x.Type)
+            .HasColumnName("Type")
+            .HasColumnType("smallint")
+            .IsRequired();
+
+        builder.Property(x => x.Status)
+            .HasColumnName("Status")
+            .HasColumnType("smallint")
+            .IsRequired();
+
+        builder.Property(x => x.CreatedAt)
+            .HasColumnName("CreatedAt")
+            .HasColumnType("timestamptz")
+            .IsRequired();
+
+        builder.Property(x => x.UpdatedAt)
+            .HasColumnName("UpdatedAt")
+            .HasColumnType("timestamptz")
+            .IsRequired();
+
+        builder.Property(x => x.Start)
+            .HasColumnName("Start")
+            .HasColumnType("timestamptz");
+
+        builder.Property(x => x.Return)
+            .HasColumnName("Return")
+            .HasColumnType("timestamptz");
+
+        builder.Property(x => x.Active)
+            .HasColumnName("Active")
+            .HasColumnType("boolean")
+            .IsRequired();
+    }
+}
---
+++ GearOps.Api/Migrations/20251014225046_InitialDeploy.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GearOps.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialDeploy : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Devices",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "varchar(30)", nullable: false),
+                    Type = table.Column<short>(type: "smallint", nullable: false),
+                    Status = table.Column<short>(type: "smallint", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamptz", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "timestamptz", nullable: false),
+                    Start = table.Column<DateTime>(type: "timestamptz", nullable: true),
+                    Return = table.Column<DateTime>(type: "timestamptz", nullable: true),
+                    Active = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Devices", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Devices");
+        }
+    }
+}
