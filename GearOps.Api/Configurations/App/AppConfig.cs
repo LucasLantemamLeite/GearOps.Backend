2cacfa69c688dec08f4a2806d553bf654a3f0d5b
Lucas Lantemam Leite <lucaslantemam2005@gmail.com>
2025-10-14 20:04:59 -0300

    feat: adiciona um middleware para caso uma exceção vaze sempre caia em um retorno status 500

--- GearOps.Api/Configurations/App/AppConfig.cs
+++ GearOps.Api/Configurations/App/AppConfig.cs
@@ -4,6 +4,21 @@ public static class AppConfig
 {
     public static WebApplication ApplyConfiguration(this WebApplication app)
     {
+        app.UseHttpsRedirection();
+        app.UseRouting();
+
+        app.UseExceptionHandler(appError =>
+        {
+
+            appError.Run(async context =>
+            {
+                context.Response.StatusCode = 500;
+                context.Response.ContentType = "application/json";
+
+                var response = new { Message = "Erro interno no servidor. Tente novamente mais tarde." };
+                await context.Response.WriteAsJsonAsync(response);
+            });
+        });
 
         if (app.Environment.IsDevelopment())
         {
@@ -12,6 +27,8 @@ public static class AppConfig
         }
 
         app.UseHealthChecks("/v1/health");
+
+        app.MapControllers();
 
         return app;
     }
