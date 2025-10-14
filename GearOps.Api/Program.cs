e8ee69fc6de97cf8736834dcaa0e6ecf327a0054
Lucas Lantemam Leite <lucaslantemam2005@gmail.com>
2025-10-14 19:56:06 -0300

    feat: adiciona a configuração do projeto

--- GearOps.Api/Program.cs
+++ GearOps.Api/Program.cs
@@ -1,6 +1,15 @@
+using GearOps.Api.Configurations.App;
+using GearOps.Api.Configurations.Builder;
+using GearOps.Api.Configurations.Services;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.ApplyConfiguration();
+
+builder.Services.RegisterServices(builder.Configuration);
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.ApplyConfiguration();
 
 app.Run();
