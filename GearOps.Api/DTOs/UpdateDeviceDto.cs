e1b1952529eae67fcf7a7171a53cf122cb3892a2
Lucas Lantemam Leite <lucaslantemam2005@gmail.com>
2025-10-14 21:32:09 -0300

    feat: adiciona o Dto para atualizar informações de máquinas

---
+++ GearOps.Api/DTOs/UpdateDeviceDto.cs
@@ -0,0 +1,27 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace GearOps.Api.DTOs;
+
+public record UpdateDeviceDto
+{
+    [Required(ErrorMessage = "O identificador do dispositivo é obrigatório.")]
+    public Guid Id { get; init; }
+
+    [Required(ErrorMessage = "O nome do dispositivo é obrigatório.")]
+    [MaxLength(30, ErrorMessage = "O nome do dispositivo deve possuir no máximo 30 caracteres.")]
+    public required string Name { get; init; }
+
+    [Required(ErrorMessage = "O tipo do dispositivo é obrigatório.")]
+    [Range(1, 5, ErrorMessage = "O tipo do dispositivo não existe no servidor.")]
+    public required int Type { get; init; }
+
+    [Required(ErrorMessage = "O status do dispositivo é obrigatório.")]
+    [Range(1, 3, ErrorMessage = "O status do dispositivo não existe no servidor.")]
+    public required int Status { get; init; }
+
+    [DataType(DataType.DateTime, ErrorMessage = "O início da manutenção está no formato inválido.")]
+    public DateTime? Start { get; init; }
+
+    [DataType(DataType.DateTime, ErrorMessage = "O retorno da manutenção está no formato inválido.")]
+    public DateTime? Return { get; init; }
+}
